405e38c26b3c78788b8083a1a2a9b3eef3a7c3a0
kurta17 <114169244+kurta17@users.noreply.github.com>
2023-10-18 21:37:08 +0700

    add up,down,left, right method

--- board.py
+++ board.py
@@ -5,35 +5,66 @@ class Board():
             [2, 3, 4, 5],
             [10, 11, 12, 13],
             [1, 6, 7, 8],
-            [9, 14, 15, "."]
+            [9, 14, 15, 0]  # Use 0 to represent the empty cell
         ]
+        self.void_pos = (3, 3)
+
+    def up(self):
+        up_to_void_pos = (self.void_pos[0] - 1, self.void_pos[1])
+        self.swap_with_void(up_to_void_pos)
+        self.draw_tiles()  
     
+    def down(self):
+        up_to_void_pos = (self.void_pos[0] + 1, self.void_pos[1])
+        self.swap_with_void(up_to_void_pos)
+        self.draw_tiles()  
+
+    def left(self):
+        up_to_void_pos = (self.void_pos[0] , self.void_pos[1] -1)
+        self.swap_with_void(up_to_void_pos)
+        self.draw_tiles()
+
+    def right(self):
+        up_to_void_pos = (self.void_pos[0] , self.void_pos[1] + 1)
+        self.swap_with_void(up_to_void_pos)
+        self.draw_tiles()
+
+
+    def swap_with_void(self, int_pos):
+        x1, y1 = int_pos
+        x2, y2 = self.void_pos
+        if 0 <= x1 < len(self.tiles[1]) and 0 <= y1 < len(self.tiles[0]) :
+            if (x1 == x2 and (y1 == y2 - 1 or y1 == y2 + 1)) or ((x1 == x2 - 1 or x1 == x2 + 1) and y1 == y2):
+                self.tiles[x2][y2], self.tiles[x1][y1] = self.tiles[x1][y1], self.tiles[x2][y2]
+                self.void_pos = (x1, y1) 
+
     def draw_grid(self):
-        t.penup()
-        t.hideturtle()
-        t.speed(0)
-        
-        # Set initial position
+        grid = t.Turtle()
+        grid.penup()
+        grid.hideturtle()
+        grid.speed(0)
+
         x, y = -125, 135
         
         for row in self.tiles:
             for tile in row:
-                t.goto(x, y)
-                t.pendown()
-                t.forward(50)
-                t.right(90)
-                t.forward(50)
-                t.right(90)
-                t.forward(50)
-                t.right(90)
-                t.forward(50)
-                t.right(90)
-                t.penup()
+                grid.goto(x, y)
+                grid.pendown()
+                grid.forward(50)
+                grid.right(90)
+                grid.forward(50)
+                grid.right(90)
+                grid.forward(50)
+                grid.right(90)
+                grid.forward(50)
+                grid.right(90)
+                grid.penup()
                 x += 50
             x = -125
             y -= 50
 
     def draw_tiles(self):
+        t.clear()
         t.penup()
         t.hideturtle()
         t.goto(-100,100)
--- main.py
+++ main.py
@@ -6,6 +6,14 @@ screen.tracer(0)
 board = Board()
 board.draw_tiles()
 board.draw_grid()
+
+screen.listen()
+screen.onkey(fun=board.up , key="Up")
+screen.onkey(fun=board.down , key="Down")
+screen.onkey(fun=board.left , key="Left")
+screen.onkey(fun=board.right , key="Right")
+
+
 screen.update()
 
 t.done()
