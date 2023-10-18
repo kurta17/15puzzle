0ae02697fa01e3ddb3144df02290f0f6d007c0f8
kurta17 <114169244+kurta17@users.noreply.github.com>
2023-10-18 12:04:14 +0700

    drow grid on screen

--- board.py
+++ board.py
@@ -1,11 +1,38 @@
 import turtle as t
 class Board():
     def __init__(self):
-        self.tiles = [[2,3,4,5],
-                      [10,11,12,13],
-                      [1,6,7,8],
-                      [9,14,15,None]]
+        self.tiles = [
+            [2, 3, 4, 5],
+            [10, 11, 12, 13],
+            [1, 6, 7, 8],
+            [9, 14, 15, "."]
+        ]
     
+    def draw_grid(self):
+        t.penup()
+        t.hideturtle()
+        t.speed(0)
+        
+        # Set initial position
+        x, y = -125, 135
+        
+        for row in self.tiles:
+            for tile in row:
+                t.goto(x, y)
+                t.pendown()
+                t.forward(50)
+                t.right(90)
+                t.forward(50)
+                t.right(90)
+                t.forward(50)
+                t.right(90)
+                t.forward(50)
+                t.right(90)
+                t.penup()
+                x += 50
+            x = -125
+            y -= 50
+
     def draw_tiles(self):
         t.penup()
         t.hideturtle()
@@ -18,4 +45,4 @@ class Board():
             t.backward(200)
             t.right(90)
             t.forward(50)
-            t.left(90)
+            t.left(90)  
--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import turtle as t
 from board import Board
 screen = t.Screen()
 screen.setup(600,600)
-screen.tracer(1)
+screen.tracer(0)
 board = Board()
 board.draw_tiles()
+board.draw_grid()
 screen.update()
 
 t.done()
